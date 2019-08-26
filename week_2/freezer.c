64b4748d1d87f5a058854c27e24476c7a20a33f2
Lingke <linkegvh@gmail.com>
2019-08-26 14:35:07 +0800

    adding week 2 tutorial question 1

---
+++ week_2/freezer.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	int hrs, mins;
+	float t, T;
+	printf("Enter hours and minutes since power failure: ");
+	scanf("%d", &hrs);
+	scanf("%d", &mins);
+	
+	t = (float) hrs + ( (float) mins / 60.0);
+	T = ( (4 * pow (t, 2.0)) / (t + 2.0) ) - 20.0;
+
+	printf("Temperature in freezer = %.2f\n", T);
+
+	return 0;
+}
