d6af2ce9653f5170aea9eadee50ca017d2008e39
Lingke <linkegvh@gmail.com>
2019-10-14 12:31:31 +0800

    this is for PE1 prep

---
+++ PE1_prep/century.c
@@ -0,0 +1,15 @@
+// CS1010 AY2011/2 Semester 1 // PE1 Ex1: century.c
+// Name:Ding Lingke
+// Matriculation number:
+// plab account-id: // Discussion group: // Description:
+int main(void)
+{
+	int year; // user’s input
+	printf("Enter year: ");
+	scanf("%d", &year);
+
+	:wq
+
+
+	return 0; 
+}
