0347f7bab1425ad2c451c2fb54d27db5f0aba311
Lingke <linkegvh@gmail.com>
2019-11-26 23:00:53 +0800

    added README for final prep cos all names are similar and added a few codes

--- final_prep/test2.c
+++ final_prep/test2.c
@@ -1,14 +1,36 @@
 #include <stdio.h>
-void mystery(int, int);
+
+
+int minMax (int [], int, int *);
 
 int main (){
-    mystery(15, 3);
+    int min, max = 0;
+    int arr[] = {3, 2, 1, 4, 5};
+
+    min = minMax (arr, 5, &max);
+
+    printf("Minimum is ( %d ), ", min);
+    printf("and maximum is ( %d ).\n", max);
+    printf("This is * ( &max) %d\n", *(&max));
     return 0;
 }
 
-void mystery(int n, int b) {
-    if (n != 0) {
-        mystery(n / b, b);
-        putchar('b' + n % b);
+int minMax(int arr[], int size, int *max){
+    int i, min;
+
+    min = arr[0];
+    *max = arr[0];
+    printf("this is max: %p\n", max);
+
+    for (i = 1; i < size; i ++){
+        if (arr[i] > *max){
+            *max = arr[i];
+            printf("this is max new: %d\n", *max);
+        }else if (arr[i] < min){
+            min = arr[i];
+        }
     }
+    printf("this is max last: %p\n", max);
+    printf("this is max last: %d\n", *max);
+    return min;
 }
